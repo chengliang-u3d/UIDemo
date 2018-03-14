f0e7ec66d1a203215ced9b492c12a43b4072d3a5
ChengLiang <chengliang@unity3d.com>
2018-03-14 11:01:21 +0800

    add mouse position

---
+++ Assets/MousePos.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MousePos : MonoBehaviour {
+
+    Canvas parentCanvas;
+
+	// Use this for initialization
+	void Start () {
+         parentCanvas = GetComponentInParent<Canvas>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector2 movePos;
+
+
+        RectTransformUtility.ScreenPointToLocalPointInRectangle(
+            parentCanvas.transform as RectTransform,
+            Input.mousePosition, parentCanvas.worldCamera,
+            out movePos);
+
+        Vector3 mousePos = parentCanvas.transform.TransformPoint(movePos);
+
+
+        //Move the Object/Panel
+        transform.position = mousePos;
+	}
+}
